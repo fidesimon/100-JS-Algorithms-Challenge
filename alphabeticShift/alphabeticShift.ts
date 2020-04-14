419cfaa2c821a5d932a431a1ef28cd765b0af592
Szymon S <fide.simon@gmail.com>
2020-04-14 22:12:04 +0200

    alphabeticShift added

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,13 @@
 function alphabeticShift(inputString: string): string {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let inputStringSplitted = inputString.split('');
+    let outputString = '';
 
+    inputStringSplitted.forEach((char)=>{
+        let charIndex = alphabet.indexOf(char);
+        outputString += charIndex < alphabet.length - 1 ? alphabet[charIndex + 1] : alphabet[0];
+    });
+    return outputString;
 }
 
 console.log(alphabeticShift('crazy'));
