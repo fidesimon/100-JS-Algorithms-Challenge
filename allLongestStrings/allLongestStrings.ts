f5a709647c01c816aa4c887edaf8c4757f6982be
Szymon S <fide.simon@gmail.com>
2020-04-14 21:20:03 +0200

    allLongestStrings added

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,15 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+    let length = 0;
+    let outputArray = [];
+    inputArray.forEach((str: string) => {
+        if(length < str.length){
+            length = str.length;
+            outputArray = [str];
+        } else if (length == str.length) {
+            outputArray.push(str);
+        }
+    });
+    return outputArray;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
