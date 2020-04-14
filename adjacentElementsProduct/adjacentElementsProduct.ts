d1319317622f706fce3ceeff892ec87885b7d5f5
Szymon S <fide.simon@gmail.com>
2020-04-14 21:08:49 +0200

    adjacentElementsProduct logic added using reduce()

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,10 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+    let largestProduct = inputArray[0] * inputArray[1];
+    inputArray.reduce((a:number, b:number)=>{
+        largestProduct = largestProduct > a * b ? largestProduct : a * b;
+        return b;
+    });
+    return largestProduct;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
