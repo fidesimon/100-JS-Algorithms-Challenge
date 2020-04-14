445928d953c7eb5e204073278a68cc08cf14dd3e
Szymon S <fide.simon@gmail.com>
2020-04-14 19:40:20 +0200

    Added absoluteValuesSumMinization logic, but the readme seems a bit off. had to add .sort() for input data as the test data was wrong and results were wrong too

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
 function absoluteValuesSumMinimization(a: number[]): number {
+    const arr = a.sort();
+    const isEven = arr.length % 2 === 0;
 
+    return isEven ? arr[arr.length/2 - 1] : arr[Math.floor(arr.length/2)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
