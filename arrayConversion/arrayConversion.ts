e036a6e4f2a08e6b5fa218ba004662eb47ec2304
Szymon S <fide.simon@gmail.com>
2020-04-15 23:41:41 +0200

    arrayConversion done using recursive function

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,17 @@
 function arrayConversion(inputArray: number[]): number {
+    return recursiveConversion(inputArray, true);
+}
 
+function recursiveConversion(inputArray: number[], isSum: boolean){
+    let arrayAfterOperation: number[] = [];
+    if(inputArray.length / 2 >= 1){
+        for(let i = 0; i < inputArray.length; i+=2){
+            arrayAfterOperation.push(isSum ? inputArray[i] + inputArray[i+1] : inputArray[i] * inputArray[i+1]);
+        }
+    } else {
+        return inputArray;
+    }
+    return recursiveConversion(arrayAfterOperation, !isSum);
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
