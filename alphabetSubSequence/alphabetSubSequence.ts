b2736809b047002a2f3d8003f058378e3d47ee20
Szymon S <fide.simon@gmail.com>
2020-04-15 22:25:39 +0200

    alphabetSubSequence added - reused idea from alphabetic shift

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,12 @@
 function alphabetSubsequence(s: string): boolean {
-
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let sSplitted = s.split('');
+    for(let i = 0; i < sSplitted.length -1; i++){
+        if(alphabet.indexOf(sSplitted[i]) >= alphabet.indexOf(sSplitted[i+1])){
+            return false;
+        }
+    }
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
