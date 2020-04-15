51df37241ba4cbd6746800b382d4baf227e6c467
Szymon S <fide.simon@gmail.com>
2020-04-15 22:33:32 +0200

    alternatingSums finished

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,14 @@
 function alternatingSums(a: number[]): number[] {
+    let team1total: number = 0;
+    let team2total: number = 0;
 
+    for (let i = 0; i < a.length; i++) {
+        if ((i + 2) % 2 == 0)
+            team1total += a[i];
+        else
+            team2total += a[i];
+    }
+    return [team1total, team2total];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
