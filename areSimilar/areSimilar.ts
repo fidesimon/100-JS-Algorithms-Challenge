0c501c65e8f4f966fbacb44ce0fa1b281721f4e6
Szymon S <fide.simon@gmail.com>
2020-04-15 23:13:35 +0200

    areSimilar done

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,5 +1,17 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   
+    if (a.toString() === b.toString())
+        return true;
+    let count = 0;
+    let indexes = [];
+    for(let i = 0; i < a.length; i++){
+        if(a[i] !== b[i]){
+            count++;
+            if(count > 2)
+                return false;
+            indexes.push(i);
+        }
+    }
+    return a[indexes[0]] == b[indexes[1]] && a[indexes[1]] == b[indexes[0]];
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
