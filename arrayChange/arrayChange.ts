c7116d5079118c3659362bd1dae8e5ce1c4d2a4e
Szymon S <fide.simon@gmail.com>
2020-04-15 23:21:53 +0200

    arrayChange done

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,12 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let counter = 0;
+    for(let i = 1; i < inputArray.length; i++){
+        if(inputArray[i] <= inputArray[i-1]){
+            counter += inputArray[i-1] - inputArray[i] + 1;
+            inputArray[i]++;
+        }
+    }
+    return counter;
 }
 
 console.log(arrayChange([1, 1, 1]));
