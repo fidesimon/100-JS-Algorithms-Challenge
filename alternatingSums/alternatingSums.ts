31286c17635c72c95aa36759c445c619217099d6
Szymon S <fide.simon@gmail.com>
2020-04-15 22:42:05 +0200

    replaced for loop with foreach

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -2,12 +2,12 @@ function alternatingSums(a: number[]): number[] {
     let team1total: number = 0;
     let team2total: number = 0;
 
-    for (let i = 0; i < a.length; i++) {
-        if ((i + 2) % 2 == 0)
-            team1total += a[i];
-        else
-            team2total += a[i];
-    }
+    a.forEach((val, index)=>{
+        if (index % 2 === 0) 
+            team1total += val;
+        else 
+            team2total += val;
+    });
     return [team1total, team2total];
 }
 
