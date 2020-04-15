e477e2e772724e8b4246610c7882666971c1349a
Szymon S <fide.simon@gmail.com>
2020-04-15 22:57:33 +0200

    A bit different approach to the areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,16 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+    let me = [yourLeft, yourRight];
+    let friend = [friendsLeft, friendsRight];
+    let areEquallyStrong = true;
+    me.forEach((hand)=>{
+        if(friend.indexOf(hand) >= 0)
+            friend.slice(friend.indexOf(hand));
+        else
+            areEquallyStrong = false;
+    });
+    return areEquallyStrong;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
