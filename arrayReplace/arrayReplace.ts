291465a161cb5ab37003d73add1793ebf09780c4
Szymon S <fide.simon@gmail.com>
2020-04-18 00:00:59 +0200

    added arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,10 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    return inputArray.map((item) => {
+        if (item == elemToReplace)
+            return elemToReplace;
+        else
+            return item;
+    })
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
