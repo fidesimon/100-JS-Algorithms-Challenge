c4f8874e4b7325d204387a61654dc05cc979e14f
Szymon S <fide.simon@gmail.com>
2020-04-18 01:25:06 +0200

    caseInsensitivePalindrome done

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,7 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    let lower = inputString.toLowerCase();
+    let reversedLower = lower.split('').reverse().join('');
+    return lower == reversedLower;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
