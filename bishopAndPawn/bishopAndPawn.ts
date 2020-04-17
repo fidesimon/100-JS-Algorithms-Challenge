3a2c27f004f09e8700223e035c1541ee18a95372
Szymon S <fide.simon@gmail.com>
2020-04-18 00:30:53 +0200

    bishopAndPawn finished

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,13 @@
 function bishopAndPawn(bishop: string, pawn: string): boolean {
+    let canBishopCapturePawn = false;
+    let bishopPosition = getPiecePosition(bishop);
+    let pawnPosition = getPiecePosition(pawn);
+    return Math.abs(bishopPosition[0] - pawnPosition[0]) == Math.abs(bishopPosition[1] - pawnPosition[1]);
+}
 
+function getPiecePosition(pos: string){
+    const letterPos = ['a','b','c','d','e','f','g','h'];
+    return [letterPos.indexOf(pos.split('')[0]), +pos.split('')[1] - 1];
 }
 
 console.log(bishopAndPawn('a1', 'c3'));
