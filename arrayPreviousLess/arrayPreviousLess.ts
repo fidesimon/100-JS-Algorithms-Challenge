5276e6a74cae6ec61170189c74e950c7f75407a8
Szymon S <fide.simon@gmail.com>
2020-04-17 23:58:46 +0200

    arrayPreviousLess added

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,18 @@
 function arrayPreviousLess(items: number[]): number[] {
+    let resultArray = [-1];
+    for(let i = 1; i < items.length; i++){
+        let currentValue = items[i];
+        let val = -1;
+        for(let j = i; j > 0; j--){
+            if(items[j - 1] < currentValue){
+                val = items[j - 1];
+                break;
+            }
+        }
+        resultArray.push(val);
+    }
 
+    return resultArray;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
