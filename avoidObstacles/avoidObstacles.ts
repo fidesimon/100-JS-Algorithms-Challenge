68bc8e0630beb5b1cbc66379ffcb70983d7b8f0f
Szymon S <fide.simon@gmail.com>
2020-04-18 00:16:53 +0200

    avoidObstacles done

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,9 @@
 function avoidObstacles(inputArray: number[]): number {
-
+    let minJumpLength = inputArray.sort()[inputArray.length - 1];
+    for (let i = minJumpLength + 1; i > 0; i--) {
+        minJumpLength = inputArray.every((item) => { return item % i }) ? i : minJumpLength;
+    }
+    return minJumpLength;
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
