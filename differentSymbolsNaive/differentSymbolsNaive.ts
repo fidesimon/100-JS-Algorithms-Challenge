1289f922cfce590a6a6c21ad277ff185fafacabd
Szymon S <fide.simon@gmail.com>
2020-04-18 22:57:26 +0200

    differentSymbolsNaive added

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,14 @@
 function differentSymbolsNaive(s: string): number {
- 
+    let arr = s.split('');
+    let distinctArr = [];
+    arr.forEach((char) => {
+        if(!distinctArr.includes(char))
+            distinctArr.push(char);
+    });
+    return distinctArr.length;
+
+    //Another approach:
+    //return (new Set(s.split(''))).size;
 }
 
 console.log(differentSymbolsNaive('cabca'));
