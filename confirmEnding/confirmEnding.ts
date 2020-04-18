db7bcf7781243e288adfa0bbdd8b484a71113a98
Szymon S <fide.simon@gmail.com>
2020-04-18 02:53:19 +0200

    confirmEnding added

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,5 +1,7 @@
 function confirmEnding(str: string, target: string) {
-   
+    const targetLength = target.length;
+    const substr = str.substr(str.length - targetLength);
+    return substr === target;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
