194cee5b9851130030532f2d131baa2c12fc25dd
Szymon S <fide.simon@gmail.com>
2020-04-18 23:36:13 +0200

    domainType added

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,20 @@
 function domainType(domains: string[]): string[] {
+    let domainMap = {
+        'org': 'organization',
+        'com': 'commercial',
+        'net': 'network',
+        'info': 'info'
+    };
 
+    let resultArr = [];
+    domains.forEach((domain) => {
+        let domainSplitted = domain.split('.');
+        let dom = domainSplitted[domainSplitted.length - 1];
+        resultArr.push(domainMap[dom]);
+    });
+
+
+    return resultArr;
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
