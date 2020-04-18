25c2cc2251cbdb2a349fe13cc0a871787ceef37d
Szymon S <fide.simon@gmail.com>
2020-04-18 02:06:02 +0200

    chunkyMonkey finished

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,5 +1,13 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
-
+    let resultArray: any[][] = [];
+    while(arr.length > 0){
+        if(arr.length < size){
+            resultArray.push(arr.splice(0,arr.length));
+            break;
+        }
+        resultArray.push(arr.splice(0,size));
+    }
+    return resultArray;
 }
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
