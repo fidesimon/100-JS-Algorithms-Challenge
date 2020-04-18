4edc52e75961444fb310f67e3f9abfe6b8b934d1
Szymon S <fide.simon@gmail.com>
2020-04-18 02:35:56 +0200

    companyBotStrategy added

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,15 @@
 function companyBotStrategy(trainingData: number[][]): number {
- 
+    let sum = 0;
+    let count = 0;
+    trainingData.forEach((session) => {
+        if (session[1] === 1) {
+            sum += session[0];
+            count++;
+        }
+    });
+    return count === 0 ? 0.0 : sum / count;
 }
 
 console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
 console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+console.log(companyBotStrategy([[4, -1], [0, 0], [5, -1]]));
