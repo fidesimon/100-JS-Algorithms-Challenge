78f334c8b0e76beea9e3cb298cae0406625c3755
Szymon S <fide.simon@gmail.com>
2020-04-18 22:54:12 +0200

    depositProfit added

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,11 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+    let yearsCount = 0;
+    let currentMoney = deposit;
+    while(currentMoney < threshold){
+        yearsCount++;
+        currentMoney += currentMoney * (rate/100);
+    }
+    return yearsCount;
 }
 
 console.log(depositProfit(100, 20, 170))
