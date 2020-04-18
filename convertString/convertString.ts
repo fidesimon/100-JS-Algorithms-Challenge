c1b8041cfc56bac496f3e3d81afe01af3b264138
Szymon S <fide.simon@gmail.com>
2020-04-18 03:22:05 +0200

    convertSting added

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,5 +1,19 @@
 function convertString(s: string, t: string): boolean {
+    const tSplitted = t.split('');
+    let index = 0;
+    let isConvertPossible = true;
+    tSplitted.forEach((item)=>{
+        for(let i = index; i < s.length; i++){
+            if(s[i] === item){
+                index = i;
+            }
+        }
+        if(s[index] !== item){
+            isConvertPossible = false;
+        }
+    });
 
+    return isConvertPossible;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
