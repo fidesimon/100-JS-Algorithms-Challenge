a099161927bd306a0d1d3f5294eaaf80c76ccec7
Szymon S <fide.simon@gmail.com>
2020-04-18 23:28:30 +0200

    digitDegree added

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,27 @@
 function digitDegree(n: number): number {
-  
+    let degree = 0;
+    let sum = n;
+    do{
+        if(sum < 10){
+            return degree;
+        } else {
+            sum = getSumOfDigits(sum);
+            degree++;
+        }
+    }while(true);
+}
+
+function getSumOfDigits(n: number){
+    let str = n.toString();
+    let arr = str.split('');
+    let nArr = arr.map((s)=>{ return +s;});
+    let sum = 0;
+    sum = nArr.reduce((previousValue, currentValue) => {
+        return previousValue+currentValue;
+    });
+    return sum;
 }
 
 console.log(digitDegree(5));
-console.log(digitDegree(10));
+console.log(digitDegree(100));
 console.log(digitDegree(91));
