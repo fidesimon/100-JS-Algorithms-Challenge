a816d9f04201ece25c5f764f27b508bf1f4dc405
Szymon S <fide.simon@gmail.com>
2020-04-18 03:09:36 +0200

    containsDuplicates added

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,13 @@
 function containsDuplicates(a: number[]): boolean {
-
+    return a.length !== (new Set(a)).size;
+    /*
+    Another Approach:
+    a.sort();
+    for(let i = 0; i < a.length - 1; i++)
+        if(a[i] === a[i+1])
+            return true;
+    return false;
+    */
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
