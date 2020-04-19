e58ac0a115d31a6245c233b52bb94f9d056e8159
Szymon S <fide.simon@gmail.com>
2020-04-19 19:23:33 +0200

    firstDigit added

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,5 +1,9 @@
 function firstDigit(inputString: string): string {
-
+    let arr = inputString.split('');
+    for(let i = 0; i < arr.length; i++){
+        if(!isNaN(+arr[i]))
+            return arr[i];
+    }
 }
 
 console.log(firstDigit('var_1__Int'));
