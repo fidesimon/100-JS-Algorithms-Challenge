4bcbab15c4a4e8f7885a1f453b3f27ccdeb0ea97
Szymon S <fide.simon@gmail.com>
2020-04-19 20:45:51 +0200

    flattenArray added with recursion

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,5 +1,16 @@
 function flattenArray(arr: any[]): any[] {
-
+    let flatArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            let deeperLevelArr = flattenArray(arr[i]);
+            for(let j = 0; j < deeperLevelArr.length; j++){
+                flatArray.push(deeperLevelArr[j]);
+            }
+        } else {
+            flatArray.push(arr[i]);
+        }
+    }
+    return flatArray;
 }
 
 console.log(flattenArray([[["a"]], [["b"]]]));
