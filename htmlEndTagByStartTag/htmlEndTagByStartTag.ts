df6bec9562894a863c0ee898ee0b72dde7f6bac7
Szymon S <fide.simon@gmail.com>
2020-04-19 21:31:31 +0200

    htmlEndTagByStartTag added

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,5 +1,6 @@
 function htmlEndTagByStartTag(startTag: string): string {
-
+    const splitted = startTag.replace("<", "</").split(" ");
+    return splitted.length > 1 ? splitted[0] + ">" : splitted[0];
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
