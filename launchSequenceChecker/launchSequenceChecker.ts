310a113ebfb8321368b447ec07173a4a9542074d
Szymon S <fide.simon@gmail.com>
2020-04-19 22:56:03 +0200

    launchSequenceChecker added

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,5 +1,25 @@
 function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+    let arr = [];
+    let aggregated = systemNames.map((system, index) => {
+        return [system, stepNumbers[index]];
+    });
+    let distinctSystems = new Set(systemNames);
 
+    distinctSystems.forEach((sys) => {
+        arr.push(aggregated.filter(n => n[0] == sys).map(n => n[1]));
+    });
+
+    for (let h = 0; h < arr.length; h++) {
+        let currValue = arr[h][0];
+        for (let i = 1; i < arr[h].length; i++) {
+            if (currValue < arr[h][i])
+                currValue = arr[h][i];
+            else
+                return false;
+        }
+    }
+
+    return true;
 }
 
 console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
