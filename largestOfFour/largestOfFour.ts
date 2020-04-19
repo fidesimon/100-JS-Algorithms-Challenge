f88e23c93f420f5aec0674a5ca8f2ea3c19cef97
Szymon S <fide.simon@gmail.com>
2020-04-19 22:12:41 +0200

    largestOfFour done

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,5 +1,9 @@
 function largestOfFour(nums: number[][]): number[] {
-
+    let returnArray = [];
+    nums.forEach(arr => {
+        returnArray.push(arr.sort((a,b)=>{ return a-b; })[arr.length-1]);
+    });
+    return returnArray;
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
