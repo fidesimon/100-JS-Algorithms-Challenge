1e5cb8fbca79e6f37ab0f7bb8b0a2fa48519dcd8
Szymon S <fide.simon@gmail.com>
2020-04-19 22:59:50 +0200

    longestDigitsPrefix added

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,14 @@
 function longestDigitsPrefix(inputString: string): string {
-
+    let split = inputString.split('');
+    let result = "";
+    for(let i = 0; i<split.length; i++){
+        if(!isNaN(+split[i])){
+            result = result.concat(split[i]);
+        } else {
+            break;
+        }
+    }
+    return result;
 }
 
 console.log(longestDigitsPrefix('123aa1'));
