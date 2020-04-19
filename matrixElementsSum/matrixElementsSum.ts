ac4d9366e3f36d1ae3c1034b55290a9e6e612874
Szymon S <fide.simon@gmail.com>
2020-04-19 23:23:23 +0200

    matrixElementsSum added

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,5 +1,16 @@
 function matrixElementsSum(matrix: any[][]): number {
+    let sum = 0;
+    let hauntedIndexes = [];
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[i].length; j++){
+            if(matrix[i][j] == 0 && !hauntedIndexes.includes(j))
+                hauntedIndexes.push(j);
+            else if (!hauntedIndexes.includes(j))
+                sum += matrix[i][j];
+        }
+    }
 
+    return sum;
 }
 
 console.log(matrixElementsSum(
