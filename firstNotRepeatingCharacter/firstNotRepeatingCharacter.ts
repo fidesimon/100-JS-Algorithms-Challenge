d89c6cc115dab93be6e5492681f6e037803ae3e3
Szymon S <fide.simon@gmail.com>
2020-04-19 20:14:40 +0200

    firstNotRepeatingCharacter added

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,5 +1,17 @@
 function firstNotRepeatingCharacter(s: string): string {
+    let obj: object = {};
+    let arr = [];
+    let splitted = s.split('');
+    for (let i = 0; i < splitted.length; i++) {
+        if (obj.hasOwnProperty(splitted[i])) {
+            return firstNotRepeatingCharacter(s.replace(new RegExp(splitted[i], "g"), ""));
 
+        } else {
+            obj[splitted[i]] = splitted[i];
+            arr.push(splitted[i]);
+        }
+    }
+    return arr.length !== 0 ? arr[0] : '_';
 }
 
 console.log(firstNotRepeatingCharacter('abacabad'));
