82f707b9f55b660bde4a183a1c5ec8ae094e95e1
Szymon S <fide.simon@gmail.com>
2020-04-19 23:06:07 +0200

    makeArrayConsecutive2 added

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,13 @@
 function makeArrayConsecutive2(statues: number[]): number {
-
+    let numberOfAdditionalStatues = 0;
+    statues.sort((a, b) => (a - b));
+    const min = statues[0];
+    const max = statues[statues.length - 1];
+    for (let i = min + 1; i < max; i++) {
+        if (!statues.includes(i))
+            numberOfAdditionalStatues++;
+    }
+    return numberOfAdditionalStatues;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
