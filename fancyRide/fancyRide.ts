7dd964c3fcf33b010231fe667da8578871947cd5
Szymon S <fide.simon@gmail.com>
2020-04-19 18:47:18 +0200

    fancyRide added

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,8 @@
 function fancyRide(l: number, fares: number[]): string {
-
+    let prices = fares.map((ride)=>{
+        return ride*l;
+    });
+    return prices[4] <= 20 ? "UberSUV" : prices[3] <= 20 ? "UberBlack" : prices[2] <= 20 ? "UberPlus" : prices[1] <= 20 ? "UberXL" : prices[0] <= 20 ? "UberX" : "None";
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
