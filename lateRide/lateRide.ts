b68f4de379f14ced37b08f93dd49f74e9db89838
Szymon S <fide.simon@gmail.com>
2020-04-19 22:18:49 +0200

    lateRide done

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,5 +1,17 @@
 function lateRide(n: number): number {
+    const hours = Math.floor(n/60);
+    const minutes = n % 60;
+    return getSum(hours.toString()) + getSum(minutes.toString());
+}
 
+function getSum(s: string){
+    if(s.length == 1)
+        return +s;
+    let sum = 0;
+    s.split('').forEach(str => {
+        sum += +str;
+    });
+    return sum;
 }
 
 console.log(lateRide(240));
