1798e2eac9dd8e97e05e3c6668e6f481465a429b
Szymon S <fide.simon@gmail.com>
2020-04-19 18:23:31 +0200

    electionWinners done

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,14 @@
 function electionsWinners(votes: number[], k: number): number {
-
+    let electionWinners = 0;
+    let max = Math.max(...votes);
+    votes.forEach((vote) => {
+        if(vote === max)
+            electionWinners++;
+        else
+            if(max < vote + k)
+                electionWinners++;
+    })
+    return electionWinners;
 }
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
