5947c15a4fce85db45904c14fb3060cc0c2c886d
Szymon S <fide.simon@gmail.com>
2020-04-19 22:03:20 +0200

    isLucky added

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,19 @@
 function isLucky(n: number): boolean {
+    const str = n.toString();
+    const firstHalf = str.substr(0, str.length/2);
+    const secondHalf = str.substring(str.length/2);
+    const firstSum = getSum(firstHalf);
+    const secondSum = getSum(secondHalf);
     
+    return firstSum === secondSum;
+}
+
+function getSum(str: string){
+    let sum = 0;
+    str.split('').forEach(n=>{
+        sum += +n;
+    });
+    return sum;
 }
 
 console.log(isLucky(1230));
