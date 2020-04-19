952ae6c21acdaf494258ee18a9a89cd9dd823e23
Szymon S <fide.simon@gmail.com>
2020-04-19 18:30:06 +0200

    extractEachKth

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,7 @@
 function extractEachKth(inputArray:number[], k: number): number[] {
+    return inputArray.filter((item, index)=>{
+        return (index + 1) % 3 !== 0;
+    })
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
