03929230b06817264b6426bb3b6eb7c9157cceb1
Szymon S <fide.simon@gmail.com>
2020-04-19 21:13:39 +0200

    growingPlant added

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,13 @@
 function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
-
+    let dayCount = 0;
+    let sum = 0;
+    while (sum < desiredHeight) {
+        dayCount++;
+        sum += upSpeed;
+        if (sum < desiredHeight)
+            sum -= downSpeed;
+    }
+    return dayCount;
 }
 
 console.log(growingPlant(100, 10, 910))
