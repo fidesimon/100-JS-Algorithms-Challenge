44df35386b25f105da4d97be9610513594ed5c7f
Szymon S <fide.simon@gmail.com>
2020-04-19 21:57:08 +0200

    integerToStringOfFixedWidth finished

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,10 @@
 function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+    const strVal = number.toString();
+    if (strVal.length >= width) {
+        return strVal.substr(strVal.length - width);
+    } else {
+        return "0".repeat(width - strVal.length) + strVal;
+    }
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
