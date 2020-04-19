76912c9519e770eea461da018bb355b985452bdf
Szymon S <fide.simon@gmail.com>
2020-04-19 22:06:30 +0200

    isTandemRepeat done

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,5 +1,8 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+    let strLength = inputString.length;
+    if (strLength % 2 !== 0)
+        return false;
+    return inputString.substr(0, strLength/2) === inputString.substr(strLength/2);
 }
 
 console.log(isTandemRepeat('tandemtandem'))
