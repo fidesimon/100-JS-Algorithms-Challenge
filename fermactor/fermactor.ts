8401a746c5a9f8d394a5496cd4af811b35586b1a
Szymon S <fide.simon@gmail.com>
2020-04-19 19:01:10 +0200

    fermactor added

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,12 @@
 function fermactor(n: number): number[] {
-
+    for(let i = 1; i < n; i++){
+        for(let j = 1; j < i; j++){
+            if(Math.pow(i,2) - Math.pow(j,2) === n){
+                return [i, j];
+            }
+        }
+    }
+    return [];
 }
 
 console.log(fermactor(15));
