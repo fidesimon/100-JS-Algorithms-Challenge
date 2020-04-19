8ddb7d1c6404a9a8b5e8949b54343f6fe0fcb4c9
Szymon S <fide.simon@gmail.com>
2020-04-19 19:17:57 +0200

    findClosestPair added

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,5 +1,16 @@
 function findClosestPair(numbers: number[], sum: number): number {
-   
+    let closestPairDistance: number;
+    numbers.forEach((num, index) => {
+        for (let i = index; i < numbers.length; i++) {
+            if (Math.abs(num) + Math.abs(numbers[i]) == sum) {
+                if (closestPairDistance == null || closestPairDistance > i - index) {
+                    closestPairDistance = i - index;
+                }
+            }
+        }
+    });
+
+    return closestPairDistance == null ? -1 : closestPairDistance;
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
