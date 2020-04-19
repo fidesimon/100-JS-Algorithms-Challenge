400db751e9bb40a864fcd893715ff038effbfb83
Szymon S <fide.simon@gmail.com>
2020-04-19 19:39:35 +0200

    firstDuplicate added

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,11 @@
 function firstDuplicate(a: number[]): number {
-    
+    let obj = {};
+    for (let i = 0; i < a.length; i++){
+        if(obj.hasOwnProperty(a[i]))
+            return a[i];
+        obj[a[i]] = a[i];
+    }
+    return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
