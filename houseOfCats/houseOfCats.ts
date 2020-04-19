ac1522842deee4887928d209e47bb78497c00037
Szymon S <fide.simon@gmail.com>
2020-04-19 21:27:31 +0200

    houseOfCats added

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,5 +1,9 @@
 function houseOfCats(legs: number): number[] {
-
+    let ppl = [];
+    for(let i = legs; i >= 0; i-= 4){
+        ppl.push(i/2);
+    }
+    return ppl;
 }
 
 console.log(houseOfCats(6));
