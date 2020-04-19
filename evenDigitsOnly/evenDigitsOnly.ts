7e8c1542730e090abfccc37da339898687adf4ab
Szymon S <fide.simon@gmail.com>
2020-04-19 18:27:58 +0200

    evenDigitsOnly added

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,5 +1,8 @@
 function evenDigitsOnly(n: number): boolean {
-
+    let strArr = n.toString().split('');
+    return strArr.every((s)=>{
+        return +s % 2 == 0;
+    });
 }
 
 console.log(evenDigitsOnly(248622));
