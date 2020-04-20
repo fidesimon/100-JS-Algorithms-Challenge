910ee3ee603968ff2e5def2ae1f97ab8b52e2f17
Szymon S <fide.simon@gmail.com>
2020-04-20 22:47:47 +0200

    newNumeralSystem added

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,12 @@
 function newNumeralSystem(number: string): string[] {
+    let chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const numberVal = chars.indexOf(number);
+    let retArr = [];
+    for(let i = 0; i <= Math.ceil(numberVal/2); i++){
+        retArr.push(`${chars[i]} + ${chars[numberVal - i]}`);
+    }
 
-}   
+    return retArr;
+}
 
 console.log(newNumeralSystem('G'));
