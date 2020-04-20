1bb2e8226ed69e23bbc8e714bfff9775e94250d9
Szymon S <fide.simon@gmail.com>
2020-04-20 23:19:44 +0200

    pagesNumberingWithInk added

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,3 +1,20 @@
 function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+    while (numberOfDigits >= 0) {
+        let length = current.toString().length;
 
+        if (length < numberOfDigits) {
+            numberOfDigits -= length;
+            let nextLength = (current + 1).toString().length;
+            if (nextLength > numberOfDigits)
+                return current;
+            current++;
+        } else {
+            numberOfDigits -= length;
+        }
+
+    }
+    return current;
 }
+
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
