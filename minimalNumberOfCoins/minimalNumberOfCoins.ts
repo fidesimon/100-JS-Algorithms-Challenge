98a638eabe7567226454001d243d43dc78dfded9
Szymon S <fide.simon@gmail.com>
2020-04-20 21:34:25 +0200

    minimalNumberOfCoins added

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,14 @@
 function minimalNumberOfCoins(coins: number[], price: number): number {
-
+    let count = 0;
+    coins.sort((a, b) => (a - b));
+    for (let i = coins.length - 1; i >= 0; i--) {
+        let floor = Math.floor(price/coins[i]);
+        if(floor > 0){
+            count += floor;
+            price -= coins[i] * floor;
+        }
+    }
+    return count;
 }
 
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
