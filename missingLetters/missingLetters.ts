d3986b5b05cd0026459fc0a7230ddd3687ae3534
Szymon S <fide.simon@gmail.com>
2020-04-20 22:01:22 +0200

    missingLetters added

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,5 +1,16 @@
 function missingLetters(str: string): string {
-   
+    let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    const splittedStr = str.split('');
+    let first = chars.indexOf(splittedStr[0]);
+
+    for(let i = 0; i < splittedStr.length; i++){
+        if(chars.indexOf(splittedStr[i]) != first){
+            return chars[first];
+        }
+        first++;
+    }
+    return undefined;
 }
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
