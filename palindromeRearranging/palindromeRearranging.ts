4b7cd81503850b2d2f6fa49b77391fc8cd107a64
Szymon S <fide.simon@gmail.com>
2020-04-21 22:19:33 +0200

    added a case for when there is an odd number of characters

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -2,18 +2,20 @@ function palindromeRearranging(inputString: string): boolean {
     let obj = {};
     let strSplitted = inputString.split('');
     for (let i = 0; i < strSplitted.length; i++) {
-        if(obj.hasOwnProperty(strSplitted[i]))
+        if (obj.hasOwnProperty(strSplitted[i]))
             obj[strSplitted[i]]++;
         else
             obj[strSplitted[i]] = 1;
     }
 
-    for(let str in obj){
-        if(obj[str] % 2 !== 0) 
-            return false;
+    let countOddChars = 0;
+
+    for (let str in obj) {
+        if (obj[str] % 2 !== 0)
+            countOddChars++;
     }
 
-    return true;
+    return countOddChars > 1 ? false : true;
 }
 
 console.log(palindromeRearranging('aabb')); 
