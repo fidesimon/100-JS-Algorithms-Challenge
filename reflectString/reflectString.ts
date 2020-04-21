5113ff38adce8b424d27adbdaec591ed53b75930
Szymon S <fide.simon@gmail.com>
2020-04-21 23:02:30 +0200

    reflectString added

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,12 @@
 function reflectString(inputString: string): string {
-  
-}
+    const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const charsLength = chars.length;
 
-console.log(reflectString("name"));
+    let retStr = '';
+    for (let i = 0; i < inputString.length; i++) {
+        let index = chars.indexOf(inputString[i]);
+        retStr += chars[charsLength - index - 1];
+    }
+
+    return retStr;
+}
