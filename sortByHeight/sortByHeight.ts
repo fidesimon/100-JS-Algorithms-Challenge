3e5181c09547b412bfd91d4d482afd9f2ce7b27a
Szymon S <fide.simon@gmail.com>
2020-04-21 23:37:51 +0200

    sortByHeight added

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,14 @@
 function sortByHeight(a: number[]): number[] {
-
+    let sorted = a.filter(n => n !== -1).sort((a, b) => a - b);
+    let index = 0;
+    a = a.map(n => {
+        if (n !== -1) {
+            index++;
+            return sorted[index - 1];
+        }
+        return n;
+    });
+    return a;
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
