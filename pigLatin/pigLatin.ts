baec5c5c43f86a8fd3584e2a896bdc060185a826
Szymon S <fide.simon@gmail.com>
2020-04-21 22:33:54 +0200

    pigLatin done

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,5 +1,16 @@
 function pigLatin(str: string): string {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let firstVowelIndex = -1;
+    const isFirstCharAVowel = vowels.indexOf(str[0]) >= 0 ? true : false;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.indexOf(str[i]) >= 0) {
+            firstVowelIndex = i;
+            break;
+        }
+    }
 
+    let retWord = str.substr(firstVowelIndex) + str.substr(0, firstVowelIndex);
+    return retWord + (isFirstCharAVowel ? "way" : "ay");
 }
 
 console.log(pigLatin("glove"));
