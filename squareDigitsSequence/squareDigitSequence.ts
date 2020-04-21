c2afe63b8281e270d96701bb64c8371210daf82e
Szymon S <fide.simon@gmail.com>
2020-04-22 00:02:09 +0200

    squareDigitSequence added

--- squareDigitsSequence/squareDigitSequence.ts
+++ squareDigitsSequence/squareDigitSequence.ts
@@ -1,5 +1,26 @@
 function squareDigitsSequence(a0: number): number {
+    let arrOfSequences = [a0];
+    let currentValue = a0;
+    while(true){
+        currentValue = getSquareSum(currentValue);
+        if(arrOfSequences.indexOf(currentValue) >= 0){
+            arrOfSequences.push(currentValue);
+            break;
+        }
 
+        arrOfSequences.push(currentValue);
+    }
+
+    return arrOfSequences.length;
+}
+
+function getSquareSum(n: number){
+    let str = n.toString().split('');
+    let sum = 0;
+    str.forEach(digit => {
+        sum += Math.pow(+digit, 2);
+    });
+    return sum;
 }
 
 
