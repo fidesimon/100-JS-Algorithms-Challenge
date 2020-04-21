25767be174f4cc6735fef5655e4ba5f8a33d449a
Szymon S <fide.simon@gmail.com>
2020-04-21 22:45:22 +0200

    proCategorization added

--- proCategorization/proCategorization.ts
+++ proCategorization/proCategorization.ts
@@ -1,5 +1,23 @@
 function proCategorization(pros: string[], preferences: string[][]): string[][][] {
+    let objPreferences = {};
+    let arrPreferences = [];
 
+    for(let i = 0; i < pros.length; i++){
+        for(let j = 0; j < preferences[i].length; j++){
+            if(objPreferences.hasOwnProperty(preferences[i][j])){
+                objPreferences[preferences[i][j]].push(pros[i]);
+            } else {
+                objPreferences[preferences[i][j]] = [pros[i]];
+            }
+        }
+    }
+
+    for(let key in objPreferences){
+        arrPreferences.push([key, objPreferences[key]]);
+    }
+
+    arrPreferences.sort();
+    return arrPreferences;
 }
 
 console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
