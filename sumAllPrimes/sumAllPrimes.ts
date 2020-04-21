423769a063914eb7c134dee81720947be0b7a5b0
Szymon S <fide.simon@gmail.com>
2020-04-22 00:22:15 +0200

    sumAllPrimes added; it can be still greatly improved

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,5 +1,17 @@
 function sumAllPrimes(num: number): number {
-
+    let sum = 2;
+    for(let i = 3; i <= num; i+=2){
+        let flag = false;
+        for(let j = 3; j < Math.floor(i/2); j+=2){
+            if(i % j === 0){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            sum+=i;
+    }
+    return sum;
 }
 
 console.log(sumAllPrimes(10));
