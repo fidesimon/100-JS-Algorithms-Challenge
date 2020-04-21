970c04e7092663b83dd4d0d86752cc2d4c04cfd0
Szymon S <fide.simon@gmail.com>
2020-04-21 22:17:07 +0200

    palindromeRearranging added

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,19 @@
 function palindromeRearranging(inputString: string): boolean {
+    let obj = {};
+    let strSplitted = inputString.split('');
+    for (let i = 0; i < strSplitted.length; i++) {
+        if(obj.hasOwnProperty(strSplitted[i]))
+            obj[strSplitted[i]]++;
+        else
+            obj[strSplitted[i]] = 1;
+    }
 
+    for(let str in obj){
+        if(obj[str] % 2 !== 0) 
+            return false;
+    }
+
+    return true;
 }
 
-console.log(palindromeRearranging('aabb'));
+console.log(palindromeRearranging('aabb')); 
