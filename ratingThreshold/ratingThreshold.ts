8e775b5c7f0864cfbf467b25546a403dc8764a3c
Szymon S <fide.simon@gmail.com>
2020-04-21 22:56:55 +0200

    ratingThreshold added

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,14 @@
 function ratingThreshold(threshold: number, ratings: number[][]): number[] {
-
+    let prosToReview = [];
+    ratings.forEach((rating, index) => {
+        let sum = rating.reduce((previousValue, currentValue) => { return currentValue + previousValue });
+        if (sum / rating.length < threshold)
+            prosToReview.push(index);
+    });
+    return prosToReview;
 }
 
-console.log(ratingThreshold(3.5, 
+console.log(ratingThreshold(3.5,
     [[3, 4],
     [3, 3, 3, 4],
-    [4]]));
+    [4]])); 
