807101b90d21d60e35dadc4b6d542a90f6afe794
Szymon S <fide.simon@gmail.com>
2020-04-22 21:58:26 +0200

    mostFrequentDigitSum added

--- mostFrequentDigitSum/mostFrequentDigitSum.ts
+++ mostFrequentDigitSum/mostFrequentDigitSum.ts
@@ -1,5 +1,30 @@
 function mostFrequentDigitSum(n: number): number {
+    let obj = {};
+    let x = n;
+    while (x > 0) {
+        if (obj.hasOwnProperty(x)) {
+            obj[x]++;
+        } else {
+            obj[x] = 1;
+        }
+        x = x - sum(x);
+    }
 
+    let mostFrequent = '';
+    let count = 0;
+    for (let key in obj) {
+        if (obj[key] > count) {
+            mostFrequent = key;
+            count = obj[key];
+        }
+
+    }
+    return +mostFrequent;
+}
+
+function sum(x: number) {
+    let sArr = x.toString().split('');
+    return +sArr.reduce((p, c) => { return ((+p) + (+c)).toString() });
 }
 
 console.log(mostFrequentDigitSum(88));
