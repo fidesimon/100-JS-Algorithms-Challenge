9727b8717051b1637dd40cf2c739e3b85554bcb6
Szymon S <fide.simon@gmail.com>
2020-04-22 22:19:22 +0200

    crossingSum added

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,12 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+    let rowSum = matrix[a].reduce((p, c) => (p + c));
+    let colSum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        colSum += matrix[i][b];
+    }
+    return rowSum + colSum - matrix[a][b];
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 1, 3));
