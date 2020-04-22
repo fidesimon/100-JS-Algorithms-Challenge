407c8855b15b857d3edfa2155e86c3b22e64aa0a
Szymon S <fide.simon@gmail.com>
2020-04-22 21:41:02 +0200

    uniqueDigitsProducts added

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,17 @@
 function uniqueDigitProducts(a: number[]): number {
+    let distinctArr = [];
+    for (let i = 0; i < a.length; i++) {
+        let prod = product(a[i]);
+        if (!distinctArr.includes(prod))
+            distinctArr.push(prod);
+    }
+
+    return distinctArr.length;
+}
+
+function product(n: number) {
+    let sArr = n.toString().split('');
+    return +sArr.reduce((p, q) => { return ((+p) * (+q)).toString(); })
 }
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
