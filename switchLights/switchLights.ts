c7a22e6d7f4d08dbdc297015b4acf6384ff3f4cd
Szymon S <fide.simon@gmail.com>
2020-04-22 23:05:32 +0200

    switchLights added

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,5 +1,13 @@
 function switchLights(a: number[]): number[] {
-
+    let arr = a.map(n => { return n === 1; });
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) {
+            for (let j = 0; j <= i; j++) {
+                arr[j] = !arr[j];
+            }
+        }
+    }
+    return arr.map(n => { return n ? 1 : 0 });
 }
 
 
