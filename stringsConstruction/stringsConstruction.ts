3babe9c447852d33b59a7aef8e9345c9627f863c
Szymon S <fide.simon@gmail.com>
2020-04-22 22:59:50 +0200

    stringsConstruction added

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,11 @@
 function stringsConstruction(a: string, b: string): number {
-
+    let arr: number[] = [];
+    let aSplitted = a.split('');
+    let bSplitted = b.split('');
+    for (let i = 0; i < aSplitted.length; i++) {
+        arr.push(bSplitted.filter(n => n === aSplitted[i]).length);
+    }
+    return Math.min(...arr);
 }
 
 console.log(stringsConstruction('abc', 'abccba'));
