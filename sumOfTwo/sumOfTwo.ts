8554b7598782f9cac781003e2d5576f0c44eb975
Szymon S <fide.simon@gmail.com>
2020-04-22 21:32:25 +0200

    sumOfTwo added

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,12 @@
 function sumOfTwo(a: number[], b: number[], v: number): boolean {
-
+    let filteredArr = b.filter(n => v - a[a.length - 1] <= n);
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < filteredArr.length; j++) {
+            if (a[i] + filteredArr[j] == v)
+                return true;
+        }
+    }
+    return false;
 }
 
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
