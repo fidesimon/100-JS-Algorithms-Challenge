d3760a344ecb5b086f06d03501efc8ef6e973823
Szymon S <fide.simon@gmail.com>
2020-04-22 22:09:07 +0200

    sumOddFibonacciNums added

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,5 +1,20 @@
 function sumOddFibonacciNums(num: number): number {
+    let oddFibonacci = [1, 1];
+    let fibonacci = [0, 1, 1, 2];
+    let count = 1;
+    while (oddFibonacci[count] <= num) {
+        let nextVal = fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1];
+        if (nextVal % 2 !== 0) {
+            if (nextVal > num) {
+                break;
+            }
+            oddFibonacci.push(nextVal);
+            count++;
+        }
+        fibonacci.push(nextVal);
+    }
 
+    return oddFibonacci.reduce((p, c) => (p + c));
 }
 
 console.log(sumOddFibonacciNums(1000));
